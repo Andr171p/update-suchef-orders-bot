c1e852cc55c09de6a60dcf7a126471475280fab8
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-01-31 20:47:48 +0500

    [suchef-orders-status-bot] dev: 31.01.2025 new dialog crud method

--- src/database/crud/dialog.py
+++ src/database/crud/dialog.py
@@ -4,6 +4,7 @@ if TYPE_CHECKING:
     from src.database.manager import DatabaseManager
 
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
 from src.database.interfaces import BaseCRUD
 from src.database.provider import get_database_manager
@@ -15,7 +16,7 @@ class DialogCRUD(BaseCRUD):
             self,
             manager: "DatabaseManager" = get_database_manager()
     ) -> None:
-        self._manager = get_database_manager()
+        self._manager = manager
 
     async def create(self, dialog: Dialog) -> Dialog:
         async with self._manager.session() as session:
@@ -23,6 +24,16 @@ class DialogCRUD(BaseCRUD):
             await session.commit()
             await session.refresh(dialog)
         return dialog
+
+    async def read_by_user_id(self, user_id: int) -> Sequence[Dialog]:
+        async with self._manager.session() as session:
+            stmt = (
+                select(Dialog)
+                .where(Dialog.user_id == user_id)
+                .options(selectinload(Dialog.user))
+            )
+            dialogs = await session.execute(stmt)
+        return dialogs.scalars().all()
 
     async def read_all(self) -> Sequence[Dialog]:
         async with self._manager.session() as session:
--- src/database/interfaces/crud.py
+++ src/database/interfaces/crud.py
@@ -1,4 +1,4 @@
-from typing import Sequence
+from typing import Sequence, Any
 from abc import ABC, abstractmethod
 
 from src.database.base import ModelType
@@ -7,6 +7,10 @@ from src.database.base import ModelType
 class BaseCRUD(ABC):
     @abstractmethod
     async def create(self, model: ModelType) -> ModelType:
+        raise NotImplemented
+
+    @abstractmethod
+    async def read_by_user_id(self, user_id: int) -> Any:
         raise NotImplemented
 
     @abstractmethod
